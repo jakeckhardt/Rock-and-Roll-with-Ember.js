4e79eac1b295534a50bb6ea6d0c3e4b6eca15918
Jake Eckhardt <jake.eckhardt@secondstreet.com>
2019-12-20 15:07:49 -0600

    Tests for band model have passed.

--- tests/unit/models/band-test.js
+++ tests/unit/models/band-test.js
@@ -1,13 +1,29 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Model | band', function(hooks) {
+module('Unit | Model | Band', function(hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('it exists', function(assert) {
+  test('#isGreatBand', function(assert) {
     let store = this.owner.lookup('service:store');
-    let model = store.createRecord('band', {});
-    assert.ok(model);
+
+    let pearlJam = store.createRecord('band', {
+      songs: [
+        store.createRecord('song', { title: 'Daughter', rating: 5 }),
+        store.createRecord('song', { title: 'Rearviewmirror', rating: 4 }),
+        store.createRecord('song', { title: 'Who You Are', rating: 2 }),
+      ]
+    });
+
+    assert.ok(pearlJam.isGreatBand, 'A band with two or more good songs is a great band');
+
+    let stiltskin = store.createRecord('band', {
+      songs: [
+        store.createRecord('song', { title: 'Inside', rating: 5 }),
+      ]
+    });
+    assert.notOk(stiltskin.isGreatBand, 'A band with less than two good songs is not a great band');
+
   });
 });
