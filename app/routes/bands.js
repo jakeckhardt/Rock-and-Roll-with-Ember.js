4e601ed53eebecf85047a840b381cffed53d2bcb
Jake Eckhardt <jake.eckhardt@secondstreet.com>
2020-01-07 12:29:49 -0600

    Finished Ch 12. Created templates for error and loading state.

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,9 +1,15 @@
 import Route from '@ember/routing/route';
+import wait from 'rarwe/utils/wait';
+import { action } from "@ember/object";
 
 export default Route.extend({
 
-    model() {
+  async model() {
+    await wait(3000);
+    return this.store.findAll('band');
+  },
 
-        return this.store.findAll('band');
-    }
+  // loading: action(function() {
+  //   window.alert("Loading the band's songs, OK?");
+  // }),
 });
--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,14 +1,15 @@
 import Route from '@ember/routing/route';
+import { reject } from 'rsvp';
 
 export default Route.extend({
-    model() {
-        return this.modelFor('bands.band');
-    },
+  model() {
+    return this.modelFor('bands.band');
+  },
 
-    resetController(controller) {
-        controller.setProperties({
-            isAddingSong: false,
-            newSongTitle: '',
-        });
-    },
+  resetController(controller) {
+    controller.setProperties({
+      isAddingSong: false,
+      newSongTitle: '',
+    });
+  },
 });
