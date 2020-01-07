193f32c9f353e769f2e20f72bf47ad3a4bb59329
Jake Eckhardt <jake.eckhardt@secondstreet.com>
2020-01-07 15:58:21 -0600

    Created function to update doc title based of if last letter is an S or not

---
+++ app/utils/fixDocTitle.js
@@ -0,0 +1,13 @@
+import { helper } from '@ember/component/helper';
+
+export function fixDocTitle(input) {
+  let finalLetter = input.charAt(input.length - 1);
+  
+  if(finalLetter === "s") {
+    return input + "'";
+  } else {
+    return input + "'s";
+  }
+}
+
+export default helper(fixDocTitle);
